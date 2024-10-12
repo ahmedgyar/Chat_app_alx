76013568a6de10a45f2507ad60b3f27603313bbe
ahmedgyar <ahmedyassergyar@gmail.com>
2024-10-12 23:26:06 +0300

    authentcation addition

---
+++ server/controlers/user_controler.js
@@ -0,0 +1,104 @@
+const user = require('../models/user_models');
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const userModel = require('../models/user_models');
+const cors = require('cors');
+const { use } = require('../routes/user_route');
+
+
+const createToken = (_id) => {
+    const jwtkey = process.env.JWT_SECRET_KEY;
+    return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
+};
+
+const registerUser = async(req, res) => {
+
+    try {
+        const { name, email, password } = req.body;
+
+        let user = await userModel.findOne({ email });
+
+        if (user)
+            return res.status(400).json("user with email already exits...");
+        if (!name || !email || !password)
+            return res.status(400).json("all field are required...");
+        if (!validator.isEmail(email))
+            return res.status(400).json("email must be a valid email");
+        if (!validator.isStrongPassword(password))
+            return res.status(400).json("Password must be strong");
+
+        user = new userModel({ name, email, password });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+
+        await user.save();
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ _id: user._id, name, email, token });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+
+const loginUser = async(req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await userModel.findOne({ email });
+
+        if (!user)
+            return res.status(400).json("Invalid email or password");
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword)
+            return res.status(400).json("Invalid email or password");
+
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ _id: user._id, name: user.name, email, token });
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json("Server error. Please try again.");
+    }
+
+};
+
+const findUser = async(req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        const user = await userModel.findById(userId);
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json("Server error. Please try again.");
+    }
+};
+
+const getUsers = async(req, res) => {
+
+    try {
+        const users = await userModel.find();
+
+        res.status(200).json(users);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json("Server error. Please try again.");
+    }
+};
+
+module.exports = { registerUser, loginUser, findUser, getUsers };
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user_route')
+
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+
+
+app.get("/", (req, res) => {
+    res.send("weclome our chat app user ")
+})
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URL;
+
+app.listen(port, (req, res) => {
+    console.log(`server ..running on port:${port}`);
+})
+
+mongoose.connect(`${uri}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("database connected successfully")).
+catch((err) => console.log(`failed to connect to database error: ${err}`));

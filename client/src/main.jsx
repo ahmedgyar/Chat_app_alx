91bf1f15c4a029c3b110abf0987475178f5db749
ahmedgyar <ahmedyassergyar@gmail.com>
2024-10-14 12:59:10 +0300

    change function

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthcontextProvider } from './context/AuthContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthcontextProvider>
+      <AuthContextProvider>
         <App />
-      </AuthcontextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
 
   </StrictMode>,
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap"
-import { Authcontext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
 
-  const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } = useContext(Authcontext);
+  const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } = useContext(AuthContext);
   return (
     <>
       <Form onSubmit={registerUser}>

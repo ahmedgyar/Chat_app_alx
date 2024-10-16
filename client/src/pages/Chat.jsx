1b4223f34afc2138d6f7cc58d1590f2af78b5f4a
zezo773 <zezo77328@gmail.com>
2024-10-16 12:07:43 +0300

    fetch list

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,12 +1,35 @@
 import { useContext } from "react";
+import { Container, Stack } from "react-bootstrap";
 import { ChatContext } from "../context/ChatContext";
+import UserChat from "../components/chat/UserChat";
+import { AuthContext } from "../context/AuthContext";
 
 const Chat = () => {
+  const { user } = useContext(AuthContext);
   const { userChats, isUserChatsLoading, userChatsError } =
     useContext(ChatContext);
-  console.log("UserChats", userChats);
 
-  return (<>Chat</>);
-}
+
+  return (
+    <Container>
+      {userChats?.lenght < 1 ? null : (
+        <Stack direction="horizontal" gap={4}
+          className="align-items-start">
+          <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+            {isUserChatsLoading && <p>Loading chats...</p>}
+            {userChats?.map((chat, index) => {
+              return (
+                <div key={index}>
+                  <UserChat chat={chat} user={user} />
+                </div>
+              );
+            })}
+          </Stack>
+          <p>ChatBox</p>
+        </Stack>
+      )}
+    </Container>
+  );
+};
 
 export default Chat;
